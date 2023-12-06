391c654ee29e82d53d7b0554a42af24263843823
TheAnnoying <drork2018@gmail.com>
2023-12-07 00:07:54 +0200

    feat: theme toggle

---
+++ site/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import "../app.css";
+
+	import { Sun, Moon } from "lucide-svelte";
+	import { Button } from "$lib/components/ui/button";
+	import { toggleMode } from "mode-watcher";
+</script>
+<svelte:head>
+	<title>Faster Prompting</title>
+</svelte:head>
+<Button on:click={toggleMode} variant="outline" size="icon" class="absolute top-5 right-5">
+	<Sun class="h-[1.2rem] w-[1.2rem] dark:hidden" />
+	<Moon class="h-[1.2rem] w-[1.2rem] hidden dark:block"/>
+</Button>
+<slot />
