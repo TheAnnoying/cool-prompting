a52b109c9d281cab41fc3bdd9e8564cadefd4be4
TheAnnoying <drork2018@gmail.com>
2023-12-13 14:12:48 +0200

    fix: added error handling

--- site/src/lib/store.js
+++ site/src/lib/store.js
@@ -1,24 +1,31 @@
-import { writable } from "svelte/store";
+import { writable, get } from "svelte/store";
 const serverAddress = "http://localhost:8080";
 
 export let
     result = writable(null),
     processing = writable(false),
-    prompt = writable("");
+    prompt = writable(""),
+    error = writable(false);
 
 export function clear() {
     result.set(null);
     processing.set(false);
+    error.set(false);
 }
 
 export async function submit(topic = "bananas", prompt) {
     processing.set(true);
-    prompt = prompt?.replace(/%topic/g, topic) || `Explain the following: ${topic}`;
+    prompt = prompt?.replace(/%topic/g, topic) || topic;
 
     await fetch(`${serverAddress}/generate?prompt=${encodeURIComponent(prompt)}`).then(async e => {
-        if(processing) {
+        if(get(processing)) {
             processing.set(false);
             result.set(await e.text());
         }
+    }).catch(e => {
+        processing.set(false);
+        error.set(true);
+
+        console.error(e);
     });
 }
