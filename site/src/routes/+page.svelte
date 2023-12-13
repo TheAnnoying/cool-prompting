dc218ba087655f2ff61e71eab5218060c946db5b
TheAnnoying <drork2018@gmail.com>
2023-12-13 14:14:19 +0200

    feat: prompt is saved in local storage, changed ui layout and added error handling

--- site/src/routes/+page.svelte
+++ site/src/routes/+page.svelte
@@ -1,31 +1,23 @@
 <script>
+	import { submit, clear, result, processing, error } from "../lib/store.js";
+	import { browser } from "$app/environment";
+	let topic, prompt;
+
+	import { SlidersHorizontal, Loader2 } from "lucide-svelte";
+	import { fly } from "svelte/transition";
+
 	import { Input } from "$lib/components/ui/input";
 	import { Button } from "$lib/components/ui/button";
-	import { Badge } from "$lib/components/ui/badge";
 	import { Skeleton } from "$lib/components/ui/skeleton";
 	import { Textarea } from "$lib/components/ui/textarea";
 	import * as Dialog from "$lib/components/ui/dialog";
 
-	import { SlidersHorizontal } from 'lucide-svelte';
-	import { Loader2 } from "lucide-svelte";
-	import { fly } from "svelte/transition";
-
-	import { submit, clear, result, processing } from "../lib/store.js";
-	let topic, prompt;
+	if(browser) prompt = window.localStorage.getItem("prompt") ?? "";
+	$: if(browser) window.localStorage.setItem("prompt", prompt);
 </script>
 <form class="flex flex-col gap-2 w-96">
-	<div class="relative">
-		<Badge class="absolute translate-x-1/2 -translate-y-1/2 end-0 bg-gradient-to-r from-blue-600 to-purple-500 z-10 text-white" variant="noborder">beta</Badge>
+	<div class="flex flex-row gap-2">
 		<Input type="text" placeholder="Input a topic..." disabled={$processing ? "disabled" : ""} bind:value={topic} />
-	</div>
-	<div class="flex gap-2">
-		<Button class="flex-1" variant="outline" on:click={() => {clear(); topic = ""}}>Clear</Button>
-		<Button class="flex-1" disabled={$processing ? "disabled" : ""} on:click={() => submit(topic, prompt)}>
-			{#if $processing}
-				<Loader2 class="mr-2 h-4 w-4 animate-spin" />
-			{/if}
-			Submit
-		</Button>
 		<Dialog.Root>
 			<Dialog.Trigger>
 				<Button class="flex-1" variant="outline" size="icon">
@@ -42,12 +34,26 @@
 			</Dialog.Content>
 		</Dialog.Root>
 	</div>
+	<div class="flex gap-2">
+		<Button class="flex-1" variant="outline" on:click={() => {clear(); topic = ""}}>Clear</Button>
+		<Button class="flex-1" disabled={$processing ? "disabled" : ""} on:click={() => submit(topic, prompt)}>
+			{#if $processing}<Loader2 class="mr-2 h-4 w-4 animate-spin" />{/if}
+			Submit
+		</Button>
+	</div>
 </form>
-{#if $result}
-	<div class="w-1/3 h-48 border mt-10 grid place-items-center rounded-xl px-10" transition:fly={{ y: -5 }}>{$result}</div>
-{:else if !$result && $processing}
-	<div class="w-1/3 h-48 relative grid place-items-center mt-10">
-		<p class="absolute z-10 font-mono drop-shadow-lg">generating with topic {topic ?? "bananas"}...</p>
+{#if $error}
+	<div class="min-w-[24rem] w-1/3 min-h-[12rem] border border-destructive mt-10 text-destructive flex flex-col items-center justify-center rounded-xl px-10 py-5" transition:fly={{ y: -5 }}>
+		<p>An error occured</p>
+		<a href="https://github.com/TheAnnoying/cool-prompting/issues/new" target="_blank" class="underline">Report on Github</a>
+	</div>
+{/if}
+
+{#if $result && !$processing && !$error}
+	<div class="min-w-[24rem] w-1/3 min-h-[12rem] border mt-10 grid place-items-center rounded-xl px-10 py-5" transition:fly={{ y: -5 }}>{$result}</div>
+{:else if !$result && $processing && !$error}
+	<div class="min-w-[24rem] w-1/3 h-48 relative grid place-items-center mt-10 py-5">
+		<p class="absolute z-10 font-mono">generating...</p>
 		<Skeleton class="w-full h-full"></Skeleton>
 	</div>
 {/if}
