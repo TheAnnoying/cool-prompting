ca89be4f4cf4cd4b5e183c2b567e95e2fe438025
TheAnnoying <drork2018@gmail.com>
2023-12-19 21:24:50 +0200

    feat: added api keys and queue (using lock)

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from llama_cpp import Llama
 from flask import Flask, request
+from threading import Lock
+import sqlite3
+
+lock = Lock()
+connection = sqlite3.connect("data.db", check_same_thread=False)
+cursor = connection.cursor()
+
+cursor.execute("""
+	CREATE TABLE IF NOT EXISTS keys(
+		auth_key TEXT PRIMARY KEY,
+		creation_timestamp INTEGER
+	)
+""")
 
 app = Flask(__name__)
 llm = Llama(model_path="./models/stablelm-zephyr-3b.Q4_K_M.gguf", n_threads=4, n_gpu_layers=0)
@@ -13,10 +26,17 @@ def cors_headers(response):
 
 @app.route("/generate", methods=["GET"])
 def send():
-	prompt = f"""<|user|>
-{request.args.get("prompt")}<|endoftext|>
-<|assistant|>"""
-	output = llm(prompt, max_tokens=4096)
-	return output["choices"][0]["text"]
+	prompt = f"<|user|>\n{request.args.get("prompt")}<|endoftext|>\n<|assistant|>"
+
+	cursor.execute("SELECT * from keys WHERE auth_key = ?", (request.args.get("key"),))
+	result = cursor.fetchone()
+
+	if result:
+		with lock:
+			output = llm(prompt, max_tokens=4096)
+		return output["choices"][0]["text"]
+	else:
+		return "Not authorized"
 
 app.run(port=8080)
+connection.close()
