493e189dc290f857b072d1b6a517136e390d5938
TheAnnoying <drork2018@gmail.com>
2023-12-19 23:10:05 +0200

    feat: added key validation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from llama_cpp import Llama
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from threading import Lock
 import sqlite3
 
@@ -20,15 +20,25 @@ llm = Llama(model_path="./models/stablelm-zephyr-3b.Q4_K_M.gguf", n_threads=4, n
 @app.after_request
 def cors_headers(response):
 	response.headers["Access-Control-Allow-Origin"] = "*"
-	response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+	response.headers["Access-Control-Allow-Headers"] = "Content-Type, key"
 	response.headers["Access-Control-Allow-Methods"] = "GET"
 	return response
+
+@app.route("/validatekey", methods=["GET"])
+def validate():
+	cursor.execute("SELECT * from keys WHERE auth_key = ?", (request.headers.get("key"),))
+	result = cursor.fetchone()
+
+	if result:
+		return jsonify({ "valid": True })
+	else:
+		return jsonify({ "valid": False })
 
 @app.route("/generate", methods=["GET"])
 def send():
 	prompt = f"<|user|>\n{request.args.get("prompt")}<|endoftext|>\n<|assistant|>"
 
-	cursor.execute("SELECT * from keys WHERE auth_key = ?", (request.args.get("key"),))
+	cursor.execute("SELECT * from keys WHERE auth_key = ?", (request.headers.get("key"),))
 	result = cursor.fetchone()
 
 	if result:
